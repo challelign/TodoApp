8f9f5bb01f48440bad4abea71e76e544ec1678e0
Challelign Tilahun <chalie2123@gmail.com>
2025-07-31 09:48:32 +0300

    11  Authenticate Requests Completed

--- routers/users.py
+++ routers/users.py
@@ -55,12 +55,13 @@ async def get_user (user:user_dependency, db:db_dependency):
 
 @router.patch('/password',status_code=status.HTTP_204_NO_CONTENT)
 async def change_password(user:user_dependency, db:db_dependency, user_verification:UserVerification):
-        if user is None:
-            raise HTTPException(status_code=401, detail="Authentication Failed")
-        user_model = db.query(models.Users).filter(models.Users.id ==user.get('id')).first()
-        if not bcrypt_context.verify(user_verification.password, user_model.hashed_password):
-            raise HTTPException(status_code=401, detail="Error on Password change")
-        user_model.hashed_password = bcrypt_context.hash(user_verification.new_password)
-        
-        db.add(user_model)        
-        db.commit()
+    
+    if user is None:
+        raise HTTPException(status_code=401, detail="Authentication Failed")
+    user_model = db.query(models.Users).filter(models.Users.id ==user.get('id')).first()
+    if not bcrypt_context.verify(user_verification.password, user_model.hashed_password):
+        raise HTTPException(status_code=401, detail="Error on Password change")
+    user_model.hashed_password = bcrypt_context.hash(user_verification.new_password)
+    
+    db.add(user_model)        
+    db.commit()
