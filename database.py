c2fe31f7cc6e5320c6777ad99b14177fc2dddef2
Challelign Tilahun <chalie2123@gmail.com>
2025-07-31 10:35:48 +0300

    12 - Large Production Database Setup

--- database.py
+++ database.py
@@ -2,10 +2,22 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-sqlite_file_name = "todosApp.db"
-sqlite_database_url = f"sqlite:///{sqlite_file_name}"
 
-engine = create_engine(sqlite_database_url, connect_args={"check_same_thread": False})
+# # FOR SQLITE DB CONNECTION
+
+# sqlite_file_name = "todosApp.db"
+# SQLALCHEMY_DATABASE_URL = f"sqlite:///{sqlite_file_name}"
+# engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}) 
+
+# # FOR PostgreSQL DB CONNECTION               
+# # SQLALCHEMY_DATABASE_URL = "postgresql://[username]://[password]@localhost/[database_name]"
+# SQLALCHEMY_DATABASE_URL = "postgresql://postgres://postgres@localhost/PythonTodoApplication"
+
+
+# # FOR MYSQL DB CONNECTION
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:chalie123@127.0.0.1:3306/PythonTodoApplication"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
